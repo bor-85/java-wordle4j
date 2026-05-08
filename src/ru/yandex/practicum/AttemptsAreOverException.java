2ad14315b552e2c866535dcccb61c08bf00a95f3
Bor <Bor_85@inbox.ru>
2026-05-08 15:41:47 +0700

    Спринт 7. Финальное задание Правки по стилю 3

--- src/ru/yandex/practicum/AttemptsAreOverException.java
+++ src/ru/yandex/practicum/AttemptsAreOverException.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum;
 
 public class AttemptsAreOverException extends Exception {
-    public AttemptsAreOverException (String message) {
+    public AttemptsAreOverException(String message) {
         super(message);
     }
 }
